9ad6795d288e0e14db368e2439279f33edcdead1
DragonistYJ <1121498469@qq.com>
2021-11-27 10:39:02 +0800

    split and merge sentence

--- router/sentence.py
+++ router/sentence.py
@@ -5,8 +5,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.functions import mode
 from db.db_connection import engine
 from db.table import Sentence
+from snowflake import IdWorker
 
 router = APIRouter(prefix="/sentence")
+
+sentence_id_worker = IdWorker(0, 1, 0)
 
 
 @router.get("/query")
@@ -16,7 +19,7 @@ async def query(sentence_id: str = None, belong_document_id: str = None, page: i
     if sentence_id is not None:
         query = query.filter(Sentence.sentence_id == sentence_id)
     if belong_document_id is not None:
-        query = query.filter(Sentence.belong_document_id == belong_document_id)
+        query = query.filter(Sentence.belong_document_id == belong_document_id).order_by(Sentence.sequence)
     if page >= 0 and size >= 0:
         query = query.limit(size).offset(page * size)
     sentence = query.all()
@@ -44,6 +47,48 @@ async def update(model: UpdateModel):
     return {"status": "ok"}
 
 
+@router.post("/split")
+async def split(sentence_id: int, word_id: int):
+    """
+    第word_id个拆分
+    如[1,2,3,4,5] word_id=2拆分为
+    [1,2],[3,4,5]
+    """
+    session = sessionmaker(engine)()
+    sentence = session.query(Sentence).filter(Sentence.sentence_id == sentence_id).first()
+    new_sentence = Sentence(sentence_id=sentence_id_worker.get_id(),
+                            belong_document_id=sentence.belong_document_id,
+                            context=sentence.context[word_id:],
+                            sequence=sentence.sequence + 1)
+    for i, token in enumerate(new_sentence.context):
+        token["word_id"] = i
+    sentence.context = sentence.context[:word_id]
+    session.query(Sentence).filter(Sentence.belong_document_id == sentence.belong_document_id,
+                                   Sentence.sequence > sentence.sequence).update(
+                                       {Sentence.sequence: Sentence.sequence + 1})
+    session.add(new_sentence)
+    session.commit()
+    session.close()
+    return {"status": "ok"}
+
+
+@router.post("/merge")
+async def merge(belong_document_id: int, sequence1: int, sequence2: int):
+    session = sessionmaker(engine)()
+    sentence1 = session.query(Sentence).filter(Sentence.belong_document_id == belong_document_id, Sentence.sequence == sequence1).first()
+    sentence2 = session.query(Sentence).filter(Sentence.belong_document_id == belong_document_id, Sentence.sequence == sequence2).first()
+    length = len(sentence1.context)
+    for token in sentence2.context:
+        token["word_id"] += length
+    sentence1.context += sentence2.context
+    session.query(Sentence).filter(Sentence.sentence_id == sentence1.sentence_id).update({Sentence.context: sentence1.context})
+    session.query(Sentence).filter(Sentence.sentence_id == sentence2.sentence_id).delete()
+    session.query(Sentence).filter(Sentence.belong_document_id == belong_document_id, Sentence.sequence > sequence2).update({Sentence.sequence: Sentence.sequence - 1})
+    session.commit()
+    session.close()
+    return {"status": "ok"}
+
+
 class AddedSentence(BaseModel):
     belong_document_id: str
     sequence: int
@@ -62,6 +107,17 @@ async def add(added_sentence: AddedSentence):
                                    Sentence.sequence >= added_sentence.sequence).update(
                                        {Sentence.sequence: Sentence.sequence + 1})
     session.add(sentence)
+    session.commit()
+    session.close()
+    return {"status": "ok"}
+
+
+@router.post("/delete")
+async def delete(sentence_id: int):
+    session = sessionmaker(engine)()
+    sentence = session.query(Sentence).filter(Sentence.sentence_id == sentence_id).first()
+    session.query(Sentence).filter(Sentence.sentence_id == sentence_id).delete()
+    session.query(Sentence).filter(Sentence.belong_document_id == sentence.belong_document_id, Sentence.sequence >= sentence.sequence).update({Sentence.sequence: Sentence.sequence - 1})
     session.commit()
     session.close()
     return {"status": "ok"}
